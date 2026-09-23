e5f5a5ae303f681dbeb8c816efdd270edfd6b77f
sky04808 <sky04808@gmail.com>
2026-09-23 02:33:51 +0000

    project1

--- chap3/prob1/main.c
+++ chap3/prob1/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
-int main(){
-	printf("Hello World!\n");
+#include "copy.h"
+
+char line[MAXLINE];
+char longest[MAXLINE];
+
+main() {
+	int len;
+	int max;
+	max = 0;
+
+	while(gets(line) != NULL) {
+		len = strlen(line);
+		if(len > max) {
+			max = len;
+			copy(line, longest);
+		}
+	}
+	if(max > 0)
+		printf("%s \n", longest);
 	return 0;
 }
